fcec57cf87902c1d02e6a46dd601f34ba64e2fac
AlexseyLoginov <devileean@gmail.com>
2022-12-06 00:05:26 +0300

    first steps in unity , added move forward and jumped

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed; // скорость передвижения
+    public float jumpForce; // сила прыжка
+    public Rigidbody2D rb;
+
+    private void Update()
+    {
+        //transform.Translate(transform.right * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.D))//двигаемся по вектору вперед по оси х
+        {
+            rb.velocity = new Vector2(speed, rb.velocity.y);//двигаемся по вектору вперед по оси х
+            //transform.Translate(transform.right * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))//двигаемся по вектору назад по оси х
+        {
+            //transform.Translate(-transform.right * speed * Time.deltaTime);
+            rb.velocity = new Vector2(-speed, rb.velocity.y);//двигаемся по вектору назад по оси х
+        }
+        if (Input.GetKeyDown(KeyCode.Space))//добавляем прыжок
+        {
+            rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse); //созда]м нашу силу одним импульсом
+        }
+    }
+
+}
